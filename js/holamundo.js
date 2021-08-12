f0f35a79a3ffc9b942c3845c02358ba700b2aea6
maria <marianavarroabraham@gmail.com>
2021-08-12 15:25:17 -0300

    agrego index y primera lcase de js

---
+++ js/holamundo.js
@@ -0,0 +1,36 @@
+//Esto es un comentario simple de una sola linea
+
+/* Esto es un 
+comentario de 
+multiples lineas */
+
+//mostrar un mje en consola
+console.log('Hola comision 16i');
+
+//mostrar un mje en html
+document.write('Este es mi primer mensaje');
+
+//mostrar un mje en una ventana emergente
+alert('Hola Mundo');
+
+//crear una variable (var-let-cons)
+
+let comision = '16i';
+
+document.write(' <br> comision: ' + comision);
+
+//modificar el contendio de una variable
+
+comision = 'i';
+
+document.write(' <br> comision: ' + comision);
+
+const anio = 2021;
+document.write(' <br> año: ' + anio);
+
+//se rompe la linea de codigo porque es una constante no se puede cambiar el valor
+anio = 2022;
+document.write(' <br> año: ' + anio);
+
+//hacer tarea del campus 
+
